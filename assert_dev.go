015d8014a9804f8367918d4a14036f4643801302
kyrylokhlopko <khlopko@gmail.com>
2024-10-13 22:31:07 +0200

    Factor out panic into a separate function

--- assert_dev.go
+++ assert_dev.go
@@ -6,43 +6,46 @@ import "fmt"
 
 func Assert(condition func() bool, msg string) {
 	if !condition() {
-		panic(fmt.Sprintf("Assetion failed: %s", msg))
+		failWithMsg(msg)
 	}
 }
 
 func Equal(a any, b any, msg string) {
 	if a != b {
-		panic(fmt.Sprintf("Assetion failed: %s", msg))
+		failWithMsg(msg)
 	}
 }
 
 func NotEqual(a any, b any, msg string) {
 	if a == b {
-		panic(fmt.Sprintf("Assetion failed: %s", msg))
+		failWithMsg(msg)
 	}
 }
 
 func Nil(a any, msg string) {
 	if a != nil {
-		panic(fmt.Sprintf("Assetion failed: %s", msg))
+		failWithMsg(msg)
 	}
 }
 
 func NotNil(a any, msg string) {
 	if a == nil {
-		panic(fmt.Sprintf("Assetion failed: %s", msg))
+		failWithMsg(msg)
 	}
 }
 
 func Err(err error, msg string) {
 	if err == nil {
-		panic(fmt.Sprintf("Assetion failed: %s", msg))
+		failWithMsg(msg)
 	}
 }
 
 func NoErr(err error, msg string) {
 	if err != nil {
-		panic(fmt.Sprintf("Assetion failed: %s", msg))
+		failWithMsg(msg)
 	}
 }
 
+func failWithMsg(msg string) {
+	panic(fmt.Sprintf("Assetion failed: %s", msg))
+}
